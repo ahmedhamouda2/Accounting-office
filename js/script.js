86d2e7eaf9dddc02f9791f67849e6834aae123cb
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2022-01-19 21:58:24 +0200

    part three from create sections for homepage

--- js/script.js
+++ js/script.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
 	$(".custom a").click(function(){
 		$(this).addClass("active").siblings().removeClass("active")
 	});
+	// $( ".image-last" ).hover(
+	// 	function() {
+	// 		$('.image-left-center').hide();
+	// 	}, function() {
+	// 		$('.image-left-center').show();
+	// 	}
+	//   );
+		$(window).scroll(function(){
+			if($(window).scrollTop() == 0 ){
+				$('.block').addClass("active").siblings().removeClass("active");
+			}
+			var scrollToTop = $(".scroll-top");
+			if ($(window).scrollTop() >= 1000) {
+				scrollToTop.fadeIn(300);
+			} else{
+				scrollToTop.fadeOut(300);
+			}
+			scrollToTop.click(function(event){
+			event.preventDefault();
+				$("html,body").stop().animate({scrollTop:0},100)
+			});
+		});
 });
