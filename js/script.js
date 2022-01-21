c8a51b3a444943cdaed6788fef68b82bc795a2a0
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2022-01-21 17:25:03 +0200

    add style typer for the main title

--- js/script.js
+++ js/script.js
@@ -2,26 +2,32 @@ $(document).ready(function(){
 	$(".custom a").click(function(){
 		$(this).addClass("active").siblings().removeClass("active")
 	});
-	// $( ".image-last" ).hover(
-	// 	function() {
-	// 		$('.image-left-center').hide();
-	// 	}, function() {
-	// 		$('.image-left-center').show();
-	// 	}
-	//   );
-		$(window).scroll(function(){
-			if($(window).scrollTop() == 0 ){
-				$('.block').addClass("active").siblings().removeClass("active");
-			}
-			var scrollToTop = $(".scroll-top");
-			if ($(window).scrollTop() >= 1000) {
-				scrollToTop.fadeIn(300);
-			} else{
-				scrollToTop.fadeOut(300);
-			}
-			scrollToTop.click(function(event){
-			event.preventDefault();
-				$("html,body").stop().animate({scrollTop:0},100)
-			});
+	$(window).scroll(function(){
+		if($(window).scrollTop() == 0 ){
+			$('.block').addClass("active").siblings().removeClass("active");
+		}
+		var scrollToTop = $(".scroll-top");
+		if ($(window).scrollTop() >= 1000) {
+			scrollToTop.fadeIn(300);
+		} else{
+			scrollToTop.fadeOut(300);
+		}
+		scrollToTop.click(function(event){
+		event.preventDefault();
+			$("html,body").stop().animate({scrollTop:0},100)
 		});
+	});
+
+	var theText = $(".typer").data("text"),
+    theTextLength = theText.length,
+    n = 0 ,
+    theTyper = setInterval(function(){
+		$(".typer").each(function(){
+			$(this).html($(this).html() + theText[n]);
+		});
+		n += 1; 
+		if(n >= theTextLength){
+			clearInterval(theTyper);
+		}
+    },120);
 });
